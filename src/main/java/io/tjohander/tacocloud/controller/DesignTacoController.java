e7296a12d151ed41731666cf13435b23a5303362
lopan455 <tjohander@gmail.com>
2021-11-01 07:39:57 -0500

    Design View and Controller GET method complete

---
+++ src/main/java/io/tjohander/tacocloud/controller/DesignTacoController.java
@@ -0,0 +1,45 @@
+package io.tjohander.tacocloud.controller;
+
+import io.tjohander.tacocloud.model.Ingredient;
+import io.tjohander.tacocloud.model.Taco;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Slf4j
+@Controller
+@RequestMapping("/design")
+public class DesignTacoController {
+
+    @GetMapping
+    public String showDesignForm(Model model) {
+        List<Ingredient> ingredients = Arrays.asList(
+                new Ingredient("FLTO", "Flour Totrilla", Ingredient.Type.WRAP),
+                new Ingredient("HDSH", "Hard Shell Corn", Ingredient.Type.WRAP),
+                new Ingredient("GRBF", "Ground Beef", Ingredient.Type.PROTEIN),
+                new Ingredient("CARN", "Carnitas", Ingredient.Type.PROTEIN),
+                new Ingredient("LTCE", "Lettuce", Ingredient.Type.VEGGIES),
+                new Ingredient("TOMT", "Tomato", Ingredient.Type.VEGGIES),
+                new Ingredient("SLSA", "Salsa", Ingredient.Type.SAUCE),
+                new Ingredient("JACK", "Monterrey Jack", Ingredient.Type.CHEESE)
+        );
+        Ingredient.Type[] types = Ingredient.Type.values();
+        for (Ingredient.Type type : types) {
+            model.addAttribute(type.toString().toLowerCase(), filterByType(ingredients, type));
+        }
+        model.addAttribute("design", new Taco());
+        return "design";
+    }
+
+    private Object filterByType(List<Ingredient> ingredients, Ingredient.Type type) {
+        return ingredients.stream()
+                .filter(x -> x.getType().equals(type))
+                .collect(Collectors.toList());
+    }
+}
