205da772979cc838e40b7e11c6abe07be896972b
Thomas Johander <tjohander@gmail.com>
2021-11-24 15:03:45 -0600

    Bootstrap 5 Homepage Navbar and Headline Completed

--- src/main/java/io/tjohander/tacocloud/Configuration/WebConfiguration.java
+++ src/main/java/io/tjohander/tacocloud/Configuration/WebConfiguration.java
@@ -1,6 +1,7 @@
 package io.tjohander.tacocloud.Configuration;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -10,6 +11,11 @@ public class WebConfiguration implements WebMvcConfigurer {
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/").setViewName("home");
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**").addResourceLocations("classpath:/static/");
     }
 }
 
